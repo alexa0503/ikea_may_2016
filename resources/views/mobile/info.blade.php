6f3e2dd3fa20dac5a1fa1933e0310ce97bfe2106
wang4869 <wang4869@139.com>
2016-06-04 12:56:21 +0800

    share bans update

--- resources/views/mobile/info.blade.php
+++ resources/views/mobile/info.blade.php
@@ -5,10 +5,23 @@
     	<img src="{{$info->thumb}}" class="shareGif" style="display:none;" width="398" height="398">
 
         <div class="shareVideo" style="display:none;"></div>
+        
+        <div class="shareBtns2">
+        	<div class="innerDiv">
+            	<a href="javascript:void(0);" class="abs ssb1" onClick="ga('send','event','UGC Recruitment','Share','ShareInSharePage_Weibo_WeChat');showShareWx();"><img src="{{asset('mobile/images/space.gif')}}" width="85" height="85"></a>
+            	<a href="javascript:void(0);" onClick="ga('send','event','UGC Recruitment','Share','ShareInSharePage_Weibo');" class="abs ssb2 sinaShare"><img src="{{asset('mobile/images/space.gif')}}" width="85" height="85"></a>
+                <a href="javascript:void(0);" onClick="ga('send','event','UGC Recruitment','Share','ShareInSharePage_Weibo_Douban');" class="abs ssb3 douban"><img src="{{asset('mobile/images/space.gif')}}" width="85" height="85"></a>
+                <a href="javascript:void(0);" onClick="ga('send','event','UGC Recruitment','Share','ShareInSharePage_Weibo_Qzone');" class="abs ssb4 qzoneShare"><img src="{{asset('mobile/images/space.gif')}}" width="85" height="85"></a>
+            </div>
+        </div>
 
         <a href="{{url('/')}}" class="abs shareBtn1"><img src="{{asset('mobile/images/shareBtn1.png')}}"></a>
     </div>
 </div>
+
+<div class="popBg" style="display:none;" onClick="closeShareWx();"></div>
+<img src="{{asset('mobile/images/shareNote.png')}}" style="display:none;" class="shareNote" onClick="closeShareWx();">
+
 @endsection
 @section('scripts')
 <script>
